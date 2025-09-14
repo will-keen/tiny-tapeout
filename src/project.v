c1d440d06a2daf633d307cab76a963043ff9e858
Will Keen <will@fractile.ai>
2025-09-14 11:42:06 +0200

    instantiate module, populate info.yaml

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_will_keen_solitaire (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -20,6 +20,14 @@ module tt_um_example (
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
+
+  solitaire u_solitaire(
+    .clk(clk),
+    .rst_n(rst_n),
+    .piece_x(ui_in[2:0]),
+    .piece_y(ui_in[5:3]),
+    .direction(ui_in[7:6])
+  );
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
